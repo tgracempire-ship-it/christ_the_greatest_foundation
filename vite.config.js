eb90c3ae00f36df604c9769a472400841689f2dd
tgracempire-ship-it <tgracempire@gmail.com>
2026-08-25 11:28:32 +0100

    Install Tailwind, Framer Motion, and Aceternity Container Scroll Animation

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     cssTarget: 'safari14',
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
